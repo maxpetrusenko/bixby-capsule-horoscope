e650c3cf656e0ad022343504d6b26496d31b1196
max petrusenko <max_p@MacBook-Pro-2.local>
2019-06-10 08:18:16 -0700

    update

--- code/SearchHor.js
+++ code/SearchHor.js
@@ -2,6 +2,9 @@ var http = require('http')
 var console = require('console')
 
 module.exports.function = function searchHor (sign) {
+  
+    // var user = '604455';
+    // var api = '0096d683aa8ecf18ea6f4ae27dc54e6d';
 
     var options = { 
     format: 'json',
@@ -12,6 +15,16 @@ module.exports.function = function searchHor (sign) {
       }
     }
 
+//   unirest.post("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aries&day=today")
+// .header("X-RapidAPI-Host", "sameer-kumar-aztro-v1.p.rapidapi.com")
+// .header("X-RapidAPI-Key", "fa8e615af3mshe43bc1e20b3e43fp1a0bd1jsn5e67a3865edd")
+// .header("Content-Type", "application/x-www-form-urlencoded")
+// .end(function (result) {
+//   console.log(result.status, result.headers, result.body);
+// });
+  
+  
+  // var response = http.post(config.get('https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=') + sign + '&day=today',options);
    var response = http.postUrl('https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=' + sign + '&day=today',{},options);
   console.log(sign);
   response.sign = sign;
